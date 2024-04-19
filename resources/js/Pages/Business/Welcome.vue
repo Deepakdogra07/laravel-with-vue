291b52ec0d1ca4c2a6ebdd0866d5623eb5363147
Deepak6672 <deepakvisionvivante@gmail.com>
2024-04-19 13:17:10 +0530

    Updates on business listing

---
+++ resources/js/Pages/Business/Welcome.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import Header from '../Pages/Frontend/Header.vue';
+import Footer from '../Pages/Frontend/Footer.vue';
+import { onMounted } from 'vue';
+
+
+
+</script>
+<template>
+  <Header />
+
+  <Footer />
+</template>
+
+<style scoped>
+</style>
