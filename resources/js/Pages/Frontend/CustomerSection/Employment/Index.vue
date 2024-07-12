dd17e27c236689d31bb574b04bfd21bffa2d55a9
alkavision123 <alkavisionvivante@gmail.com>
2024-07-12 19:55:39 +0530

    Update Index.vue

--- resources/js/Pages/Frontend/CustomerSection/Employment/Index.vue
+++ resources/js/Pages/Frontend/CustomerSection/Employment/Index.vue
@@ -282,7 +282,7 @@ function removeImage(type) {
                                     <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
                                     </svg>
                                     <div class="txt_over"><b>{{form.employer_statement.name}}</b><p>file uploaded!</p>
-                            </div>
+                            </div></p></div>
                             <p class="close_image_name">{{ image_name.employer_statement }}</p>
                         </div>
                         <div v-else class="col-12 p-0">
